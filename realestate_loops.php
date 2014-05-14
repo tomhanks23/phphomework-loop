4a82b70de413f9e597031563d4c7835645eeef19
Brig Lamoreaux <bklamoreaux@gmail.com>
2014-05-14 11:15:48 -0700

    added exercises

--- realestate_loops.php
+++ realestate_loops.php
@@ -11,4 +11,38 @@ foreach ($json_a as $value) {
 // print_r($json_a);
 
 // echo "let's see some real estate data";
+
+
+// Exercise 1
+// Print each City Name
+
+// Exercise 2
+// Print each City Name and Number of Listings
+
+// Exercise 3
+// Print total number of listing for all cities
+
+// Exercise 4
+// Find the City with the least Listings
+
+// Exercise 5
+// Find the City with the Most listing
+
+// Exercise 6 
+// Find the city with the most expensive listings
+
+// Exercise 7 
+// Find the City with the least expensive Listing
+
+// Exercise 8 
+// Find the City with the oldest people
+
+// Exercise 9
+// Find the City with the Youngest and Richest City
+
+
+
+
+
+
 ?>
